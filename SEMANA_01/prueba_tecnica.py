3ba200d6dad9b28bdbbd37410554f7739a8fbb74
donjuanbruh <juanjosemicanrodriguez@gmail.com>
2026-08-01 17:03:24 +0000

    fix(main): fix main file

--- SEMANA_01/prueba_tecnica.py
+++ SEMANA_01/prueba_tecnica.py
@@ -1,4 +1,17 @@
-def __main__():
-    sum = 2+2
-    print (sum)
+# Functions
 
+def ask_IDcode():
+    id = int(input("\t Ingrese su ID estudiantil:"))
+    return id
+
+def pos_neg_zero(id):
+    if id == 0:
+        print ("\n\tSu ID estudiantil es cero")
+
+
+def __main__():
+    print ("\t\t-- REGISTRO POR ESTUDIANTE --")
+    while True:
+        
+
+__main__()
